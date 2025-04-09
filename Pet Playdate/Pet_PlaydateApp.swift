680c0d94b48186c972831aa531050c2ee2cb43a7
Michaela Verardo <25verardom@villamaria.qc.ca>
2025-04-09 15:24:23 -0400

    Initial Commit

---
+++ Pet Playdate/Pet_PlaydateApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pet_PlaydateApp.swift
+//  Pet Playdate
+//
+//  Created by Chariot 3 - Ordinateur 14 - User7 on 2025-04-09.
+//
+
+import SwiftUI
+
+@main
+struct Pet_PlaydateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
